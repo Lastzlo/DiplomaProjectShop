a4d6277ae0c02f4cdd4ba23c6aafd77c4cc8da57
Lastzlo <z.last.zlo@gmail.com>
2021-02-05 21:45:45 +0200

    feat (ProductControllerImpl.java) add @Autowired ProductService

--- src/main/java/diplomaProject/shop2/controllers/ProductControllerImpl.java
+++ src/main/java/diplomaProject/shop2/controllers/ProductControllerImpl.java
@@ -1,4 +1,15 @@
 package diplomaProject.shop2.controllers;
 
+import diplomaProject.shop2.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("product")
 public class ProductControllerImpl implements ProductController {
+
+    @Autowired
+    private ProductService productService;
+
 }
