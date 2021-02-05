c3d8bc33025f763bd29023add80da9be05b1cfa1
Lastzlo <z.last.zlo@gmail.com>
2021-02-05 23:00:58 +0200

    feat (ProductServiceImpl.saveProduct) return ProductDTO

--- src/main/java/diplomaProject/shop2/services/ProductServiceImpl.java
+++ src/main/java/diplomaProject/shop2/services/ProductServiceImpl.java
@@ -1,12 +1,22 @@
 package diplomaProject.shop2.services;
 
 import diplomaProject.shop2.dto.ProductDTO;
+import diplomaProject.shop2.model.Product;
+import diplomaProject.shop2.repos.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductServiceImpl implements ProductService {
+    @Autowired
+    ProductRepository productRepository;
+
     @Override
     public ProductDTO saveProduct (ProductDTO product) {
-        return null;
+        final Product productFromDTO = ProductDTO.toProduct(product);
+
+        final Product productFromDB = productRepository.save (productFromDTO);
+
+        return Product.toDTO (productFromDB);
     }
 }
