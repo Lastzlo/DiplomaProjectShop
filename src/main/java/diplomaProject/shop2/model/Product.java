30b6bf306a36efcc078e2c745cb31251787aaf8c
Lastzlo <z.last.zlo@gmail.com>
2021-02-05 21:30:43 +0200

    feat add Product.java

---
+++ src/main/java/diplomaProject/shop2/model/Product.java
@@ -0,0 +1,60 @@
+package diplomaProject.shop2.model;
+
+import com.fasterxml.jackson.annotation.JsonView;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    //название товара
+    private String productName;
+
+    //описание товара
+    //стандарный String ограничен в 255 charts
+    private String productDiscription;
+
+    //цена товара
+    private BigDecimal price;
+
+    public Product(){}
+
+    public Long getId () {
+        return id;
+    }
+
+    public void setId (Long id) {
+        this.id = id;
+    }
+
+    public String getProductName () {
+        return productName;
+    }
+
+    public void setProductName (String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductDiscription () {
+        return productDiscription;
+    }
+
+    public void setProductDiscription (String productDiscription) {
+        this.productDiscription = productDiscription;
+    }
+
+    public BigDecimal getPrice () {
+        return price;
+    }
+
+    public void setPrice (BigDecimal price) {
+        this.price = price;
+    }
+}
