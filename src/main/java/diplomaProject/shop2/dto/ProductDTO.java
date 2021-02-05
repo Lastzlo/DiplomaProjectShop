8ee3298b2d35ae052bea14f5c1799435c6887d98
Lastzlo <z.last.zlo@gmail.com>
2021-02-05 22:07:29 +0200

    feat add ProductDTO.java

---
+++ src/main/java/diplomaProject/shop2/dto/ProductDTO.java
@@ -0,0 +1,49 @@
+package diplomaProject.shop2.dto;
+
+import java.math.BigDecimal;
+
+public class ProductDTO {
+    private Long id;
+
+    //название товара
+    private String productName;
+
+    //описание товара
+    //стандарный String ограничен в 255 charts
+    private String productDescription;
+
+    //цена товара
+    private BigDecimal price;
+
+    public Long getId () {
+        return id;
+    }
+
+    public void setId (Long id) {
+        this.id = id;
+    }
+
+    public String getProductName () {
+        return productName;
+    }
+
+    public void setProductName (String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductDescription () {
+        return productDescription;
+    }
+
+    public void setProductDescription (String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public BigDecimal getPrice () {
+        return price;
+    }
+
+    public void setPrice (BigDecimal price) {
+        this.price = price;
+    }
+}
