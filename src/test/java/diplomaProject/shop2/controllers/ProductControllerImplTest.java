ebb2683400a2b4f70a0486dea40702a78f372168
Lastzlo <z.last.zlo@gmail.com>
2021-02-05 23:38:14 +0200

    test (ProductControllerImplTest.java) add addProduct_whenProductHaveProductNameAndProductDescriptionAndPrice

--- src/test/java/diplomaProject/shop2/controllers/ProductControllerImplTest.java
+++ src/test/java/diplomaProject/shop2/controllers/ProductControllerImplTest.java
@@ -1,12 +1,51 @@
 package diplomaProject.shop2.controllers;
 
+import diplomaProject.shop2.dto.ProductDTO;
+import diplomaProject.shop2.services.ProductService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.ResponseEntity;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.math.BigDecimal;
 
+@SpringBootTest
 class ProductControllerImplTest {
 
+    @Autowired
+    private ProductControllerImpl productController;
+
+    @MockBean
+    private ProductService productService;
+
     @Test
-    void addProduct () {
+    void addProduct_whenProductHaveProductNameAndProductDescriptionAndPrice () {
+        //given
+        final ProductDTO product = new ProductDTO (){{
+            this.setPrice (new BigDecimal (1000));
+            this.setProductDescription ("ProductDescription");
+            this.setProductName ("ProductName");
+        }};
+
+        final ProductDTO productDTO = new ProductDTO (){{
+            this.setId (10l);
+            this.setPrice (new BigDecimal (1000));
+            this.setProductDescription ("ProductDescription");
+            this.setProductName ("ProductName");
+        }};
+
+        Mockito.when (productService.saveProduct (ArgumentMatchers.any (ProductDTO.class)))
+                .thenReturn (productDTO);
+
+        //when
+        ResponseEntity<ProductDTO> productDTOResponseEntity = productController.addProduct (product);
+
+        //then
+        Assertions.assertTrue (productDTOResponseEntity.getStatusCode ().is2xxSuccessful ());
+        Assertions.assertEquals (productDTO, productDTOResponseEntity.getBody ());
     }
 }
