4d43e0f8219ce109ac611c9d402231cd9b5a1786
Lastzlo <z.last.zlo@gmail.com>
2021-02-05 22:10:54 +0200

    feat (ProductControllerImpl.java) add Logger

--- src/main/java/diplomaProject/shop2/controllers/ProductControllerImpl.java
+++ src/main/java/diplomaProject/shop2/controllers/ProductControllerImpl.java
@@ -1,6 +1,8 @@
 package diplomaProject.shop2.controllers;
 
 import diplomaProject.shop2.services.ProductService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("product")
 public class ProductControllerImpl implements ProductController {
+    private static final Logger logger = LogManager.getLogger(ProductControllerImpl.class);
 
     @Autowired
     private ProductService productService;
