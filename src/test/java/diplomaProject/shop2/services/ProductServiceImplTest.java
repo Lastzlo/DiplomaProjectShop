0e49cc8ac8086f4d176d88fa2554588318832f16
Lastzlo <z.last.zlo@gmail.com>
2021-02-05 22:59:33 +0200

    test (ProductServiceImplTest.java) saveProduct rename to saveProduct_whenProductHaveProductNameAndProductDescriptionAndPrice
    the test is successful

--- src/test/java/diplomaProject/shop2/services/ProductServiceImplTest.java
+++ src/test/java/diplomaProject/shop2/services/ProductServiceImplTest.java
@@ -1,13 +1,56 @@
 package diplomaProject.shop2.services;
 
+import diplomaProject.shop2.dto.ProductDTO;
+import diplomaProject.shop2.model.Product;
+import diplomaProject.shop2.repos.ProductRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.math.BigDecimal;
 
+@SpringBootTest
 class ProductServiceImplTest {
 
+    @Autowired
+    private ProductService productService;
+
+    @MockBean
+    private ProductRepository productRepository;
+
     @Test
-    void saveProduct () {
+    void saveProduct_whenProductHaveProductNameAndProductDescriptionAndPrice () {
+        //given
+        final ProductDTO product = new ProductDTO (){{
+            this.setPrice (new BigDecimal (1000));
+            this.setProductDescription ("ProductDescription");
+            this.setProductName ("ProductName");
+        }};
+
+        final Product productFromDB = new Product (){{
+            this.setId (10l);
+            this.setPrice (new BigDecimal (1000));
+            this.setProductDescription ("ProductDescription");
+            this.setProductName ("ProductName");
+        }};
+
+        Mockito.when (productRepository.save (
+                ArgumentMatchers.any (Product.class)
+        ))
+                .thenReturn (productFromDB);
+
+        final ProductDTO expectedProductDTO =  Product.toDTO(productFromDB);
+
+
+        //when
+        ProductDTO actualProductDTO = productService.saveProduct (product);
+
+        //then
+        Assertions.assertEquals (expectedProductDTO,actualProductDTO);
 
     }
 }
