925afa81dff3d59916695555b600940597145620
Lastzlo <z.last.zlo@gmail.com>
2021-02-06 12:04:42 +0200

    test (resources\forProductController) add products-product-after.sql and products-product-before.sql

---
+++ src/test/resources/forProductController/products-product-before.sql
@@ -0,0 +1,7 @@
+delete from product;
+
+insert into product(id, product_name, product_description, price) values
+(1, 'product_name 1', 'product_description 1', 1000);
+insert into product(id, product_name, product_description, price) values
+(10, 'product_name 10', 'product_description 10', 700.50);
+
