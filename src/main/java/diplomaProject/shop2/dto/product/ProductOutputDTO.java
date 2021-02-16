20c07ff97bb2176328335e84dcee0b63ba57801d
Lastzlo <z.last.zlo@gmail.com>
2021-02-16 18:10:11 +0200

    feat add ProductInputDTO.java and ProductOutputDTO.java
    ProductInputDTO  - use for create and update into about Product
    ProductOutputDTO - use for return into about Product

---
+++ src/main/java/diplomaProject/shop2/dto/product/ProductOutputDTO.java
@@ -0,0 +1,87 @@
+package diplomaProject.shop2.dto.product;
+
+import diplomaProject.shop2.model.Product;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class ProductOutputDTO {
+    private Long id;
+
+    //название товара
+    private String productName;
+
+    //описание товара
+    //стандарный String ограничен в 255 charts
+    private String productDescription;
+
+    //цена товара
+    private BigDecimal price;
+
+    public ProductOutputDTO (){}
+
+    public ProductOutputDTO (Long id, String productName, String productDescription, BigDecimal price) {
+        this.id = id;
+        this.productName = productName;
+        this.productDescription = productDescription;
+        this.price = price;
+    }
+
+    public static Product toProduct (ProductOutputDTO product) {
+        return new Product (
+                product.getId (),
+                product.getProductName (),
+                product.getProductDescription (),
+                product.getPrice ()
+        );
+    }
+
+    public Long getId () {
+        return id;
+    }
+
+    public void setId (Long id) {
+        this.id = id;
+    }
+
+    public String getProductName () {
+        return productName;
+    }
+
+    public void setProductName (String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductDescription () {
+        return productDescription;
+    }
+
+    public void setProductDescription (String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public BigDecimal getPrice () {
+        return price;
+    }
+
+    public void setPrice (BigDecimal price) {
+        this.price = price;
+    }
+
+
+    @Override
+    public boolean equals (Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        ProductOutputDTO that = (ProductOutputDTO) o;
+        return Objects.equals (id, that.id) &&
+                Objects.equals (productName, that.productName) &&
+                Objects.equals (productDescription, that.productDescription) &&
+                Objects.equals (price, that.price);
+    }
+
+    @Override
+    public int hashCode () {
+        return Objects.hash (id, productName, productDescription, price);
+    }
+}
