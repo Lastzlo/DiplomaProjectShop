c6cc713daabe1b60d844f70bedefbb9899af202e
Lastzlo <z.last.zlo@gmail.com>
2021-02-16 23:04:51 +0200

    refactor (ApplicationConfig) rename to AmazonS3Config and move to s3 package

--- src/main/java/diplomaProject/shop2/s3/AmazonS3Config.java
+++ src/main/java/diplomaProject/shop2/s3/AmazonS3Config.java
@@ -1,4 +1,4 @@
-package diplomaProject.shop2.config;
+package diplomaProject.shop2.s3;
 
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class ApplicationConfig {
+public class AmazonS3Config {
 
     @Value("${cloud.aws.credentials.access-key}")
     private String accessKey;
