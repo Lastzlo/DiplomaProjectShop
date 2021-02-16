c9f0f7bdc6c125ca6685b7535300de87a2a076a0
Lastzlo <z.last.zlo@gmail.com>
2021-02-16 22:01:08 +0200

    feat add PhotoService.savePhoto and Override in PhotoServiceImpl
    now PhotoServiceImpl.savePhoto always return Optional.empty ()

--- src/main/java/diplomaProject/shop2/services/PhotoService.java
+++ src/main/java/diplomaProject/shop2/services/PhotoService.java
@@ -1,8 +1,10 @@
 package diplomaProject.shop2.services;
 
-import diplomaProject.shop2.dto.ProductDTO;
+import diplomaProject.shop2.model.Photo;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
+import java.util.Optional;
 
 public interface PhotoService {
+    Optional<Photo> savePhoto (MultipartFile multipartFile);
 }
--- src/main/java/diplomaProject/shop2/services/PhotoServiceImpl.java
+++ src/main/java/diplomaProject/shop2/services/PhotoServiceImpl.java
@@ -1,12 +1,21 @@
 package diplomaProject.shop2.services;
 
+import diplomaProject.shop2.model.Photo;
 import diplomaProject.shop2.repos.PhotoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Optional;
 
 @Service
 public class PhotoServiceImpl implements PhotoService {
     @Autowired
     private PhotoRepository photoRepository;
 
+
+    @Override
+    public Optional<Photo> savePhoto (MultipartFile multipartFile) {
+        return Optional.empty ();
+    }
 }
