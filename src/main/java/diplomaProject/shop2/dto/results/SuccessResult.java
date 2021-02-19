1855170134c2b90e7a67c0248386c6731bfc7db5
Lastzlo <z.last.zlo@gmail.com>
2021-02-19 20:30:51 +0200

    refactor (SuccessResult.java) add constructor SuccessResult(description) and add constructor SuccessResult() that set description=OK

--- src/main/java/diplomaProject/shop2/dto/results/SuccessResult.java
+++ src/main/java/diplomaProject/shop2/dto/results/SuccessResult.java
@@ -1,4 +1,11 @@
 package diplomaProject.shop2.dto.results;
 
 public class SuccessResult extends ResultDTO {
+    public SuccessResult () {
+        super ("OK");
+    }
+
+    public SuccessResult (String description) {
+        super (description);
+    }
 }
