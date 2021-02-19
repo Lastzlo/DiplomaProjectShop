93b8cc1d2a0b2ec7811621924d839a2e22002ee9
Lastzlo <z.last.zlo@gmail.com>
2021-02-19 13:42:11 +0200

    feat add ResultDTO.java

---
+++ src/main/java/diplomaProject/shop2/dto/results/ResultDTO.java
@@ -0,0 +1,20 @@
+package diplomaProject.shop2.dto.results;
+
+public abstract class ResultDTO {
+    protected String description = "OK";
+
+    public ResultDTO() {
+    }
+
+    public ResultDTO(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
