f6f3f36056647de6e2cd25ece8eb8e2c5bc13aa6
Lastzlo <z.last.zlo@gmail.com>
2021-02-19 13:00:14 +0200

    feat (AmazonS3ServiceImpl.java) add saveFile and getAbsolutPathByFileName

--- src/main/java/diplomaProject/shop2/s3/AmazonS3Service.java
+++ src/main/java/diplomaProject/shop2/s3/AmazonS3Service.java
@@ -1,4 +1,9 @@
 package diplomaProject.shop2.s3;
 
+import java.io.File;
+
 public interface AmazonS3Service {
+    boolean saveFile (File file, String fileName);
+
+    String getPathByFileName (String newFileName);
 }
--- src/main/java/diplomaProject/shop2/s3/AmazonS3ServiceImpl.java
+++ src/main/java/diplomaProject/shop2/s3/AmazonS3ServiceImpl.java
@@ -2,7 +2,19 @@ package diplomaProject.shop2.s3;
 
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+
 @Service
 public class AmazonS3ServiceImpl implements AmazonS3Service{
+
+    @Override
+    public boolean saveFile (File file, String fileName) {
+        return false;
+    }
+
+    @Override
+    public String getPathByFileName (String newFileName) {
+        return null;
+    }
 
 }
