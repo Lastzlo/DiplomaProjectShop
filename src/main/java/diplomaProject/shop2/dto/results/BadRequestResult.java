2121f264b502bf78cb11330a6be028d943c9ed98
Lastzlo <z.last.zlo@gmail.com>
2021-02-19 20:27:18 +0200

    refactor (BadRequestResult.java) set description "BAD REQUEST" and add constructor BadRequestResult(description)

--- src/main/java/diplomaProject/shop2/dto/results/BadRequestResult.java
+++ src/main/java/diplomaProject/shop2/dto/results/BadRequestResult.java
@@ -2,6 +2,10 @@ package diplomaProject.shop2.dto.results;
 
 public class BadRequestResult extends ResultDTO {
     public BadRequestResult() {
-        super("JSON deserialization failed");
+        super("BAD REQUEST");
+    }
+
+    public BadRequestResult (String description) {
+        super (description);
     }
 }
