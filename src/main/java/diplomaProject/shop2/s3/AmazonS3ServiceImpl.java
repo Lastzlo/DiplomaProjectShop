2a44ed7cce3bad733c91a7299a3c70a2e515c5e8
Lastzlo <z.last.zlo@gmail.com>
2021-02-20 21:51:45 +0200

    refactor (AmazonS3ServiceImpl.java) add public saveFile(File, String) and private String getPathByFileName (String)

--- src/main/java/diplomaProject/shop2/s3/AmazonS3ServiceImpl.java
+++ src/main/java/diplomaProject/shop2/s3/AmazonS3ServiceImpl.java
@@ -1,21 +1,68 @@
 package diplomaProject.shop2.s3;
 
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.transfer.TransferManager;
+import com.amazonaws.services.s3.transfer.TransferManagerBuilder;
+import com.amazonaws.services.s3.transfer.Upload;
 import diplomaProject.shop2.dto.amazonS3.S3ServiceResultDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
+import java.util.concurrent.Executors;
 
 @Service
 public class AmazonS3ServiceImpl implements AmazonS3Service{
 
-    @Override
-    public S3ServiceResultDTO saveFile (File file, String fileName) {
-        return new S3ServiceResultDTO ();
-    }
+    @Autowired
+    AmazonS3 amazonS3Client;
+
+    @Value("${amazonProperties.endpointUrl}")
+    private String endpointUrl;
+    @Value("${amazonProperties.bucketName}")
+    private String bucketName;
+
 
     @Override
-    public String getPathByFileName (String newFileName) {
-        return null;
+    public S3ServiceResultDTO saveFile (File file, String fileName) {
+
+        int maxUploadThreads = 5;
+
+        TransferManager tm = TransferManagerBuilder
+                .standard()
+                .withS3Client(amazonS3Client)
+                .withMultipartUploadThreshold((long) (5 * 1024 * 1024))
+                .withExecutorFactory(() -> Executors.newFixedThreadPool(maxUploadThreads))
+                .build();
+
+        PutObjectRequest request =
+                new PutObjectRequest(bucketName, fileName, file)
+                        //allow to receive the file from the link
+                        .withCannedAcl(CannedAccessControlList.PublicRead);
+
+        Upload upload = tm.upload(request);
+
+        try {
+            upload.waitForCompletion();
+
+            String message = "Upload complete.";
+            String fileSrc = getPathByFileName (fileName);
+
+            return new S3ServiceResultDTO (message, fileSrc);
+        } catch (AmazonClientException | InterruptedException e) {
+            e.printStackTrace();
+
+            String message = "Error occurred while uploading file";
+            return new S3ServiceResultDTO (message);
+        }
+    }
+
+    private String getPathByFileName (String fileName) {
+        return endpointUrl + "/" + bucketName + "/" + fileName;
     }
 
 }
