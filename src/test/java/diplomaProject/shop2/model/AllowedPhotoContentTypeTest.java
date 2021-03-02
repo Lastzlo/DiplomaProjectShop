d1b5cb51a4a5308989af3594f3567c6448553608
Lastzlo <z.last.zlo@gmail.com>
2021-03-02 15:03:09 +0200

    test (AllowedPhotoContentTypeTest.java) all tests success complete

---
+++ src/test/java/diplomaProject/shop2/model/AllowedPhotoContentTypeTest.java
@@ -0,0 +1,34 @@
+package diplomaProject.shop2.model;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class AllowedPhotoContentTypeTest {
+
+    @Test
+    void getContentType () {
+        String contentType = "image/jpeg";
+
+        String jpegContentType = AllowedPhotoContentType.Jpeg.getContentType ();
+
+        Assertions.assertEquals (contentType, jpegContentType);
+    }
+
+    @Test
+    void isAllowedPhotoContentType_thenTrue () {
+        String contentType = "image/jpeg";
+
+        boolean result = AllowedPhotoContentType.isAllowedPhotoContentType (contentType);
+
+        Assertions.assertTrue (result);
+    }
+
+    @Test
+    void isAllowedPhotoContentType_thenFalse () {
+        String contentType = "its content type not is not exits in AllowedPhotoContentType";
+
+        boolean result = AllowedPhotoContentType.isAllowedPhotoContentType (contentType);
+
+        Assertions.assertFalse (result);
+    }
+}
